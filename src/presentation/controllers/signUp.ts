954d9a12cd96742257edd64dc3c607fa5384d14b
Madson Silva <madsonsilva2005@gmail.com>
2024-12-24 11:42:06 -0300

    refactor: ensure SignUpController returns 400 if no passeord is provider

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -4,8 +4,7 @@ import { BadRequest } from "../helpers/httpHelper";
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-
-    const requiredFields = ["name", "email"];
+    const requiredFields = ["name", "email", "password"];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return BadRequest(new MissingParamError(field));
